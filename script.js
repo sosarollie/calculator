677f0b4c8c9163fc09ad81ec6bdc2d2cc5c23418
18praise <jesussosarollie1812@gmail.com>
2023-01-20 17:19:58 -0300

    Make display work properly.

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
 const display = document.querySelector('#display');
 
-function populateDisplay (digit){
-    if (display.textContent == "0")
+let currentValue = "";
+let currentOperator;
+let previousValue = "";
+let erasePreviousValue = false;
+
+
+function populateDisplay (currentValue){
+    if (display.textContent == "0" || display.textContent == 0)
         display.textContent = " ";
-    display.textContent += digit;
+    display.textContent = currentValue;
 }
 
-window.onload = function() {
-    const elements = document.getElementsByClassName('numbers');
-    
-    for (const element of elements) {
-        element.addEventListener("click", e => {
-            populateDisplay(e.target.textContent);
-        })
-    }
-}
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach(number => {
+    number.addEventListener("click", n => {
+        if (erasePreviousValue == true) {
+            erasePreviousValue = false;
+            currentValue = "";
+            clearDisplay();
+        }
+        let digit = (n.target.textContent);
+        currentValue += digit;
+        console.log(currentValue)
+        populateDisplay(currentValue);
+        });
+});
+
+
+const operators = document.querySelectorAll('.operator');
+
+operators.forEach(operator => {
+    operator.addEventListener("click", o => {
+        currentOperator = o.target.textContent;
+        if (previousValue == "") {
+            previousValue = currentValue;
+        }
+        erasePreviousValue = true;
+    });
+});
 
 function add(a, b){
-    populateDisplay("+");
+    return a + b;
 };
 
 function subtract(a, b){
-    populateDisplay(a - b);
+    return a - b;
 };
 
 function divide(a, b){
-    populateDisplay(a / b);
+    return a / b;
 };
 
 function multiply(a, b){
-    populateDisplay(a * b);
+    return a * b;
 };
 
 function clearDisplay(){
@@ -37,15 +62,19 @@ function clearDisplay(){
     populateDisplay(0);
 }
 
-function operate(operator, a, b){
-    switch (operator) {
-        case 1:
-            add(a, b);
-        case 2:
-            subtract(a, b);
-        case 3:
-            divide(a, b);                    
-        case 4:
-            multiply(a, b);
+function operate(){
+    let total;
+    switch (currentOperator) {
+        case '+':
+            total = add(previousValue, currentValue);
+        case '-':
+            total = subtract(previousValue, currentValue);
+        case '/':
+            total = divide(previousValue, currentValue);                    
+        case '*':
+            total = multiply(previousValue, currentValue);
      };
+     previousValue = total;
+     clearDisplay();
+     populateDisplay(total);
 };
